9c934e90d9ee454cb73177aa64a3acf2a258e1dd
SidSan97 <sidnei1.8santiago@hotmail.com>
2020-07-08 17:56:30 -0300

    Personalização do teclado

--- forca.js
+++ forca.js
@@ -251,10 +251,10 @@ function jogar(letra){
       TECLADO VIRTUAL  
           -------------------    
 */
-	document.write("<table id=tecladao cellpadding=3 cellspacing=6 border=1 width=390 height=90 bgcolor=white style='border-radius: 5px; position: relative; margin-top: 16%; margin-left: 35.6%;'");
+	/*document.write("<table id=tecladao cellpadding=3 cellspacing=6 border=1 width=390 height=90 bgcolor=white style='border-radius: 5px; position: relative; margin-top: 16%; margin-left: 35.6%;'");
 	document.write(" style='border: 1px solid #666666;'><tr style='border: 1px #000000;'> <form name=f action=# onsubmit='return false;'>");
-var linha=0;
-for(i=65; i < 91; i++){
+var linha=0;*/
+/*for(i=65; i < 91; i++){
     if(linha == 8) {
        linha=0;
        document.write("</tr><tr style='border: 1px #000000;'>");
@@ -265,4 +265,6 @@ for(i=65; i < 91; i++){
     document.write("');document.f."+ String.fromCharCode(i)+".style.display ='none';\" class=teclado value=" + String.fromCharCode(i) + " ></td>");
     linha++;
 }
-    document.write("</tr></form></table>");
+    document.write("</tr></form></table>");*/
+
+
